64633d8679a9f89be4079448fc7b0f0db6f8bb3f
István Bozsó <bozso.istvan@csfk.mta.hu>
2018-09-07 09:45:33 +0200

    Modified utils.[sh|py]

--- utils.py
+++ utils.py
@@ -48,41 +48,53 @@ def cmd(Cmd, debug=False):
 class argp(ap.ArgumentParser):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-    
+        self.subp = None
+        
     def addargs(self, *args):
         for arg in args:
-            if arg[3] == "opt":
-                if arg[4] is not None:
-                    self.add_argument(
-                        "--{}".format(arg[0]), "-{}".format(arg[4]),
-                        help=arg[1],
-                        type=arg[5],
-                        default=arg[2],
-                        nargs=arg[7],
-                        choices=arg[6]
-                    )
-                else:
-                    self.add_argument(
-                        "--{}".format(arg[0]),
-                        help=arg[1],
-                        type=arg[5],
-                        default=arg[2],
-                        nargs=arg[7],
-                        choices=arg[6]
-                    )
-            else:
-                self.add_argument(
-                    arg[0],
-                    help=arg[1],
-                    type=arg[5],
-                    choices=arg[6]
-                )
+            _ap_add_arg(self, arg)
+
+    def subp(**kwargs):
+        if self.subp is None:
+            self.subp = self.add_subparsers(**kwargs)
     
-    def parse(self):
-        return self.parse_args()
-            
-def newarg(name, help=None, kind="pos", alt=None, type=str, choices=None,
-           default=None, nargs=None):
-    
+    def subcmd(self, name, fun, *args, **kwargs):
+        
+        subtmp = self.subp.add_parser(name, **kwargs)
+        
+        for arg in args:
+            _ap_add_arg(subtmp, arg)
+        
+        subtmp.set_defaults(func=fun)
+        
+def narg(name, help=None, kind="pos", alt=None, type=str, choices=None,
+         default=None, nargs=None):
     return (name, help, default, kind, alt, type, choices, nargs)
 
+def _ap_add_arg(obj, arg):
+    if arg[3] == "opt":
+        if arg[4] is not None:
+            obj.add_argument(
+                "--{}".format(arg[0]), "-{}".format(arg[4]),
+                help=arg[1],
+                type=arg[5],
+                default=arg[2],
+                nargs=arg[7],
+                choices=arg[6]
+            )
+        else:
+            obj.add_argument(
+                "--{}".format(arg[0]),
+                help=arg[1],
+                type=arg[5],
+                default=arg[2],
+                nargs=arg[7],
+                choices=arg[6]
+            )
+    else:
+        obj.add_argument(
+            arg[0],
+            help=arg[1],
+            type=arg[5],
+            choices=arg[6]
+        )
--- utils.sh
+++ utils.sh
@@ -1,7 +1,13 @@
 #! /usr/bin/env sh
 
+MAIN_DIR="/home/istvan/progs/utils"
+
+export PATH="$PATH:$MAIN_DIR/bin"
+export PYTHONPATH="$PYTHONPATH:$MAIN_DIR"
+
+alias sbrc='source ~/.bashrc'
+
 function update_clean {
-    
     sudo apt-get update
     sudo apt-get upgrade
     sudo apt-get dist-upgrade
@@ -10,6 +16,34 @@ function update_clean {
     sudo apt-get autoremove
 }
 
+function echoerr {
+    printf "%s\n" "$*" >&2;
+}
+
 function gstat {
     git status
+}
+
+function gpush {
+    if [[ $# -ne 1 ]]; then
+        echoerr "error: gpush: One argument (message) is required!"
+        return 1
+    fi
+    
+    git commit -am "$1"
+    git push origin master
+}
+
+function gradd {
+
+    if [[ $# -ne 1 ]]; then
+        echoerr "error: gradd: One argument (remote repository address) is required!"
+        return 1
+    fi
+
+    git remote add origin $1
+}
+
+function gpull {
+    git pull origin master
 }
