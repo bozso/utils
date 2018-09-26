be78369010344c3de90fb81c425aa2594efb88a8
István Bozsó <bozso.istvan@csfk.mta.hu>
2018-09-26 21:40:59 +0200

    Added gmt functions

---
+++ gmt_math.py
@@ -0,0 +1,115 @@
+
+class DataFile(object):
+    def __init__(self):
+        pass
+    
+    def op(self):
+        pass
+
+    def __add__(self, other):
+        op(self, other, "+")
+
+    def __sub__(self, other):
+        op(self, other, "+")
+
+    def __mul__(self, other):
+        op(self, other, "+")
+
+    def __mod__(self, other):
+        op(self, other, "+")
+
+    def __lt__(self, other):
+        op(self, other, "+")
+
+    def __le__(self, other):
+        op(self, other, "+")
+
+    def __eq__(self, other):
+        op(self, other, "+")
+
+    def __ne__(self, other):
+        op(self, other, "+")
+
+    def __gt__(self, other):
+        op(self, other, "+")
+
+    def __ge__(self, other):
+        op(self, other, "+")
+
+    def __lshift__(self, other):
+        op(self, other, "+")
+
+    def __rshift__(self, other):
+        op(self, other, "+")
+
+    def __and__(self, other):
+        op(self, other, "+")
+
+    def __or__(self, other):
+        op(self, other, "+")
+
+    def __xor__(self, other):
+        op(self, other, "+")
+
+    def __invert__(self, other):
+        op(self, other, "+")
+
+
+class MathExp(object):
+    def __init__(self, string):
+        self.string = str(string)
+    
+    def op(self, other):
+        pass
+
+    def __add__(self, other):
+        op(self, other, "+")
+
+    def __sub__(self, other):
+        op(self, other, "+")
+
+    def __mul__(self, other):
+        op(self, other, "+")
+
+    def __mod__(self, other):
+        op(self, other, "+")
+
+    def __lt__(self, other):
+        op(self, other, "+")
+
+    def __le__(self, other):
+        op(self, other, "+")
+
+    def __eq__(self, other):
+        op(self, other, "+")
+
+    def __ne__(self, other):
+        op(self, other, "+")
+
+    def __gt__(self, other):
+        op(self, other, "+")
+
+    def __ge__(self, other):
+        op(self, other, "+")
+
+    def __lshift__(self, other):
+        op(self, other, "+")
+
+    def __rshift__(self, other):
+        op(self, other, "+")
+
+    def __and__(self, other):
+        op(self, other, "+")
+
+    def __or__(self, other):
+        op(self, other, "+")
+
+    def __xor__(self, other):
+        op(self, other, "+")
+
+    def __invert__(self, other):
+        op(self, other, "+")
+
+    
+    def __str__(self):
+        return self.string
