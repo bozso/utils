9e29dbef6279c1795f7a4179eaa48c987b6a5c57
István Bozsó <bozso.istvan@csfk.mta.hu>
2019-04-05 15:02:04 +0200

    Mods: textadept

--- .textadept/init.lua
+++ .textadept/init.lua
@@ -2,7 +2,10 @@ textadept.editing.auto_indent = true
 buffer.tab_width = 4
 buffer.use_tabs = false
 
-keys["cm"] = textadept.run.compile
+keys["cb"] = textadept.run.compile
+keys["cm"] = textadept.editing.block_comment
+keys["ck"] = buffer.del_line_left
 textadept.run.compile_commands["latex"] = "latexrun %p"
+textadept.run.build_commands["latex"] = "latexrun %p"
 
 buffer:set_theme('light', {font = 'Monospace', fontsize = 14})
