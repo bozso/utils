08fa484946d9ecf7902622687b60a423b50c2fc5
István Bozsó <bozso.istvan@csfk.mta.hu>
2019-04-08 18:40:35 +0200

    Mods: textadept

--- .textadept/init.lua
+++ .textadept/init.lua
@@ -2,6 +2,34 @@ textadept.editing.auto_indent = true
 buffer.tab_width = 4
 buffer.use_tabs = false
 
+
+-- local _L = require('locale')
+
+-- local m_edit = textadept.menu.menubar[_L['_Edit']]
+-- local m_sel = m_edit[_L['_Select']]
+
+
+function xml()
+  local left, right = "<", ">"
+  buffer:begin_undo_action()
+  for i = 0, buffer.selections - 1 do
+    local s, e = buffer.selection_n_start[i], buffer.selection_n_end[i]
+    if s == e then
+      buffer:set_target_range(buffer:word_start_position(s, true),
+                              buffer:word_end_position(e, true))
+    else
+      buffer:set_target_range(s, e)
+    end
+    buffer:replace_target(left..buffer.target_text..right..left.."/"..buffer.target_text..right)
+    buffer.selection_n_start[i] = buffer.target_end
+    buffer.selection_n_end[i] = buffer.target_end
+  end
+  buffer:end_undo_action()
+end
+
+
+keys["ct"] = xml
+
 keys["ca"] = buffer.home
 keys["ce"] = buffer.line_end
 keys["am"] = textadept.run.compile
